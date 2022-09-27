d170e5585610f13f278b59d0bcd9eca0051d6805
Joan Wilson <joanwmoliveira@gmail.com>
2022-09-27 17:04:23 -0300

    chore: project creation

---
+++ TabelaDaCopa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabelaDaCopa
+//
+//  Created by Joan Wilson Oliveira on 27/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
